59bd4be6f600c6572657203943426c18c67bc2bf
Milan Desancic <milicamilan@gmail.com>
2018-06-14 00:31:00 +0200

    Povezan drools i spring

--- cdss-spring-app/src/main/java/rs/uns/ac/ftn/cdss/CdssSpringAppApplication.java
+++ cdss-spring-app/src/main/java/rs/uns/ac/ftn/cdss/CdssSpringAppApplication.java
@@ -1,7 +1,11 @@
 package rs.uns.ac.ftn.cdss;
 
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CdssSpringAppApplication {
@@ -9,4 +13,14 @@ public class CdssSpringAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CdssSpringAppApplication.class, args);
 	}
+	
+	
+	@Bean
+    public KieContainer kieContainer() {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.newKieContainer(ks.newReleaseId("rs.uns.ac.ftn","cdss-drools", "0.0.1-SNAPSHOT"));
+		KieScanner kScanner = ks.newKieScanner(kContainer);
+		kScanner.start(10_000);
+		return kContainer;
+    }	
 }
--- cdss-spring-app/src/main/java/rs/uns/ac/ftn/cdss/service/impl/TestServiceImpl.java
+++ cdss-spring-app/src/main/java/rs/uns/ac/ftn/cdss/service/impl/TestServiceImpl.java
@@ -1,5 +1,8 @@
 package rs.uns.ac.ftn.cdss.service.impl;
 
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import rs.uns.ac.ftn.cdss.service.TestService;
@@ -7,9 +10,15 @@ import rs.uns.ac.ftn.cdss.service.TestService;
 @Service
 public class TestServiceImpl implements TestService{
 
+	@Autowired
+	KieContainer kieContainer;
+	
 	@Override
 	public int test() {
-		return 8;
+		KieSession kieSession = kieContainer.newKieSession();
+        int i = kieSession.fireAllRules();
+        kieSession.dispose();
+        return i;
 	}
 
 	
