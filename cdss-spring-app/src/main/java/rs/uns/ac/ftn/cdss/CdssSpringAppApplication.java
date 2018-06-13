f1368e36459a6cf077b84439346580dfbb4618c0
Milan Desancic <milicamilan@gmail.com>
2018-06-13 21:56:37 +0200

    Inicijalna verzija Spring Boot projekta sa testnim kontrolerom

---
+++ cdss-spring-app/src/main/java/rs/uns/ac/ftn/cdss/CdssSpringAppApplication.java
@@ -0,0 +1,12 @@
+package rs.uns.ac.ftn.cdss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdssSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdssSpringAppApplication.class, args);
+	}
+}
---
+++ cdss-spring-app/src/main/java/rs/uns/ac/ftn/cdss/controllers/TestController.java
@@ -0,0 +1,16 @@
+package rs.uns.ac.ftn.cdss.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/test")
+public class TestController {
+	
+	@GetMapping()
+	public String testController() {
+		return "Milan";
+	}
+
+}
